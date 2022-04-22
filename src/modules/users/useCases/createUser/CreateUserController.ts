6cbfba1afa33c5bf7b042d231ffb45e144b2a80c
Leonardo <leonardolazzaretti3@gmail.com>
2022-04-22 13:07:29 -0300

    'done'

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -3,10 +3,18 @@ import { Response, Request } from "express";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {}
+  constructor(private createUserUseCase: CreateUserUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { name, email } = request.body
+
+    try {
+      const createNewUser = this.createUserUseCase.execute({ name, email })
+      return response.status(201).json(createNewUser)
+    } catch (e) {
+      return response.status(400).json({ error: true, message: e.message })
+    }
+
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,22 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id)
+
+    if (user === undefined) {
+      throw new Error("User doesn't exist!")
+    }
+
+    if (!user.admin) {
+      throw new Error("User is not an admin!")
+    }
+
+    const listAllUsers = this.usersRepository.list()
+
+    return listAllUsers
   }
 }
 
